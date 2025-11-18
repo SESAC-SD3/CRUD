e26e8feea25346b7be7027e8f05c5c567212683a
5chang2 <och8808@gmail.com>
2025-11-18 14:06:17 +0900

    update 기능 구현

--- src/main/java/com/example/todoapp/controller/TodoController.java
+++ src/main/java/com/example/todoapp/controller/TodoController.java
@@ -64,4 +64,30 @@ public class TodoController {
         return "redirect:/todos";
     }
 
+    @GetMapping("/todos/{id}/edit")
+    public String edit(@PathVariable Long id, Model model) {
+        TodoDto todo = todoRepository.findById(id);
+        model.addAttribute("todo", todo);
+        return "edit";
+    }
+
+    @GetMapping("/todos/{id}/update")
+    public String update(
+            @PathVariable Long id,
+            @RequestParam String title,
+            @RequestParam String content,
+            @RequestParam(defaultValue = "false") Boolean completed,
+            Model model) {
+        TodoDto todo = todoRepository.findById(id);
+
+        todo.setTitle(title);
+        todo.setContent(content);
+        todo.setCompleted(completed);
+
+        todoRepository.save(todo);
+
+        return "redirect:/todos/" + id;
+
+    }
+
 }
